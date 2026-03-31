776dbbea6c6315d56ca766655667543a61efcddf
makarroche <49964534+makarroche@users.noreply.github.com>
2026-03-31 08:43:29 -0300

    feat: helpers

--- app/utils/helpers.tsx
+++ app/utils/helpers.tsx
@@ -1,3 +1,4 @@
+import { AstralColor, AstralDirection } from "../types/mutationType"
 import { goal } from "./constants"
 
 export const parseGoalMap = () => {
@@ -10,14 +11,29 @@ export const parseGoalMap = () => {
       if (goal[i][j] === "POLYANET") {
         polyanets.push({ row: i, column: j })
       } else if (goal[i][j].includes("COMETH")) {
-        const direction = goal[i][j].split("_")[0].toLowerCase()
+        const direction = goal[i][j]
+          .split("_")[0]
+          .toLowerCase() as AstralDirection
         comeths.push({ row: i, column: j, direction })
       } else if (goal[i][j].includes("SOLOON")) {
-        const color = goal[i][j].split("_")[0].toLowerCase()
+        const color = goal[i][j].split("_")[0].toLowerCase() as AstralColor
         soloons.push({ row: i, column: j, color })
       }
     }
   }
 
   return { polyanets, comeths, soloons }
+}
+
+export const getRotationForDirection = (
+  direction: AstralDirection | undefined
+): string => {
+  const rotationMap: Record<AstralDirection, string> = {
+    up: "50deg",
+    right: "130deg",
+    down: "240deg",
+    left: "340deg",
+  }
+
+  return rotationMap[direction || "up"] || "0deg"
 }
