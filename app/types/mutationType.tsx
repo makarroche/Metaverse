8984867702532e6844b5061aad99dfe152c2435d
makarroche <49964534+makarroche@users.noreply.github.com>
2026-03-31 08:42:17 -0300

    feat: add types

--- app/types/mutationType.tsx
+++ app/types/mutationType.tsx
@@ -1,1 +1,4 @@
 export type MutationType = "POST" | "DELETE"
+export type AstralType = "polyanets" | "soloons" | "comeths"
+export type AstralDirection = "up" | "right" | "down" | "left"
+export type AstralColor = "red" | "blue" | "purple" | "white" | "green"
